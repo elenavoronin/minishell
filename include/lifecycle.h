98d24347e8872a0c31a3697221f059ebb4d19ed3
Elena Voronin <evoronin@f0r5s1.codam.nl>
2023-11-07 15:37:04 +0100

    lifecycle.h

---
+++ include/lifecycle.h
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   lifecycle.h                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: elenavoronin <elnvoronin@gmail.com>          +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/11/02 13:52:05 by evoronin      #+#    #+#                 */
+/*   Updated: 2023/11/07 15:35:58 by evoronin      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIFECYCLE_H
+# define LIFECYCLE_H
+
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdlib.h>
+
+typedef enum e_code_status
+{
+	SUCCESS,
+	MALLOC_ERROR,
+	SYNTAX_ERROR,
+    USUPORTED,
+}	t_code_status;
+
+typedef struct s_mini_env
+{
+	char	*variable_name;
+	char	*variable_path;
+}	t_mini_env;
+
+
+typedef struct s_shell_state
+{
+	int			status_code;
+	t_mini_env	**mini_env;
+}	t_shell_state;
+
+
+void			start_minishell(int argc, char **argv, char **envp);
+void			init_mini_state(t_shell_state *mini_state, char **envp);
+void			clear_mini_env(t_shell_state *mini_state);
+void			print_env_arr(t_mini_env **mini_env);
+
+#endif
