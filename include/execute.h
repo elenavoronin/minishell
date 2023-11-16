9c4070eb4ddd97170b015d681d435ec0501c6c2d
Elena Voronin <evoronin@f0r2s9.codam.nl>
2023-11-16 15:17:00 +0100

    updated path - fix leak

--- include/execute.h
+++ include/execute.h
@@ -6,7 +6,7 @@
 /*   By: elenavoronin <elnvoronin@gmail.com>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/08 14:51:43 by evoronin      #+#    #+#                 */
-/*   Updated: 2023/11/15 12:24:03 by elenavoroni   ########   odam.nl         */
+/*   Updated: 2023/11/16 15:11:17 by evoronin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ typedef int	t_pipe_fd[2];
 typedef struct s_pipes_struct
 {
 	int				nr_pipes;
-	char			*path;
+	char			**path;
 	int				*pid;
 	t_pipe_fd		*fd_arr;
 }	t_pipes_struct;
@@ -48,9 +48,11 @@ void	create_children(t_list **list, t_shell_state *mini_state,
 			t_pipes_struct *pipes);
 void	fork_cmds(char **cmd, int i, t_shell_state *mini_state,
 			t_pipes_struct *pipes);
-// int		create_dummy_cmd(t_list **list);
-char	*get_path(char **cmd, char **mini_envp, t_pipes_struct *pipes);
+char	*get_path_char(char **cmd, char **mini_envp, t_pipes_struct *pipes,
+			int cmd_nr);
+int		get_path(t_list **list, t_pipes_struct *pipes, t_shell_state *state);
 void	close_useless_pipes(int i, t_pipes_struct *pipes);
 int		redirect_stuff(int i, t_pipes_struct *pipes);
+void	clear_pipes(t_pipes_struct *pipes);
 
 #endif
