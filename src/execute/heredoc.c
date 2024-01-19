682dc183bfbcc94e6e6ef6560c53e39630b8d81c
Jelligeth <daoyi.roodt@gmail.com>
2024-01-19 14:31:47 +0100

    normifies heredoc.c

--- src/execute/heredoc.c
+++ src/execute/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: evoronin <evoronin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/01/17 12:38:52 by evoronin      #+#    #+#                 */
-/*   Updated: 2024/01/17 15:39:52 by evoronin      ########   odam.nl         */
+/*   Updated: 2024/01/19 14:29:40 by dliu          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,34 +20,22 @@ int	read_heredoc(t_cmd *cmd)
 	tmp = open("temp", O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	if (tmp == -1)
 		return (-1);
-	while (1)
+	input = readline("> ");
+	while (input && ft_strcmp(input, cmd->delimiter))
 	{
-		input = readline("> ");
-		if (input == NULL)
-			break ;
-		add_history(input);
-		if (ft_strcmp(input, cmd->delimiter) == 0)
-		{
-			free(input);
-			break ;
-		}
-		if (write(tmp, input, ft_strlen(input)) == -1) 
+		// add_history(input);
+		if (write(tmp, input, ft_strlen(input)) == -1
+			|| write(tmp, "\n", 1) == -1)
 		{
 			perror("Error writing to temporary file");
 			free(input);
 			close(tmp);
 			return (-1);
 		}
-		if (write(tmp, "\n", 1) == -1)
-		{
-			perror("Error writing newline to temporary file");
-			free(input);
-			close(tmp);
-			return (-1);
-		}
 		//expand variables
 		free(input);
+		input = readline("> ");
 	}
-	close(tmp);
+	free(input);
 	return (tmp);
 }
