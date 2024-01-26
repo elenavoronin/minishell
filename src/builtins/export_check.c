51e0b563040fca0feae79fc5b17957876ae1b99f
Jelligeth <daoyi.roodt@gmail.com>
2024-01-26 12:22:47 +0100

    fixed stupid

--- src/builtins/export_check.c
+++ src/builtins/export_check.c
@@ -6,7 +6,7 @@
 /*   By: dliu <dliu@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/21 17:21:31 by dliu          #+#    #+#                 */
-/*   Updated: 2024/01/26 11:25:58 by dliu          ########   odam.nl         */
+/*   Updated: 2024/01/26 12:22:31 by dliu          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,10 @@ int	export_check_cmds(char **cmds)
 {
 	int	i;
 
+	i = 0;
 	while (cmds && cmds[i])
 	{
-		if (!ft_isalpha(cmds[i][0] && cmds[i][0] != '_'))
+		if (ft_isalpha(cmds[i][0] == 0 && cmds[i][0] != '_'))
 		{
 			ft_perror("🐢shell: export", cmds[i], "not a valid identifier");
 			return (SYNTAX_ERROR);
